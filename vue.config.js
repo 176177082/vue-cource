0d3bbcf03a9f2de725be5aca0fbed07e6e9215dd
changxiong <176177082@qq.com>
2018-12-14 19:29:09 +0800

    第一课所有内容

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 // 保存前是否检测lint语法
+const path = require('path')
+const resolve = dir => path.join(__dirname, '')
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'iview-admin' : '/'
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false
+  // devServer: {
+  //   proxy: 'http://localhost:4000'
+  // }
 }
