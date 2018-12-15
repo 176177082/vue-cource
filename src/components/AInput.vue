018aad1e8aa4690f9bc9e23785dda80c46a677a1
changxiong <176177082@qq.com>
2018-12-15 19:03:34 +0800

    第四节课所有内容

---
+++ src/components/AInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <input v-on:input="handleInput" v-bind:value="value"/>
+</template>
+
+<script>
+  // 父子组件之间传值，是单向传送，通过。。
+  export default {
+    name: 'AInput',
+    props: {
+      value: {
+        type: [String, Number],
+        default: ''
+      }
+    },
+    methods: {
+      handleInput () {
+        const value = event.target.value
+        this.$emit('input', value)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/store.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <p>I am store</p>
+      <a-input v-model="inputValue" />
+      <a-input v-bind:value="inputValue" v-on:input="handleInput"/>
+      <p>{{inputValue}}</p>
+      <a-show v-bind:content="inputValue"/>
+    </div>
+</template>
+
+<script>
+  import AInput from '../components/AInput'
+  import AShow from '../components/AShow'
+  export default {
+    name: 'store',
+    data () {
+      return {
+        inputValue: ''
+      }
+    },
+    components: {
+      AInput,
+      AShow
+    },
+    methods: {
+      handleInput (val) {
+        this.inputValue = val
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
